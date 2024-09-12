e188e7c17cb8e693cf86a2d14989200dcc37f547
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-09-12 12:21:32 +0500

    banner cards are fixed

--- src/routes/Home/components/Mentors/components/Card/index.jsx
+++ src/routes/Home/components/Mentors/components/Card/index.jsx
@@ -2,8 +2,10 @@ import cn from "./style.module.scss";
 
 const Card = ({ type, name, about, img }) => {
   return (
-    <div style={{ backgroundImage: `url(${img})` }} className={cn.Card}>
-      <span className={cn.type}>{type}</span>
+    <div className={cn.Card_wrap}>
+      <div style={{ backgroundImage: `url(${img})` }} className={cn.Card}>
+        <span className={cn.type}>{type}</span>
+      </div>
       <div className={cn.info}>
         <h3>{name}</h3>
         <p>{about}...</p>
